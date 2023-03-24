70679ab723f2e95fafd2f73f062c6fdb4050de9f
POOJA HARIHAR <88924201+poojaharihar03@users.noreply.github.com>
2023-03-24 10:52:48 +0530

    Update summarize.R

--- summarize.R
+++ summarize.R
@@ -33,6 +33,32 @@ murders <- mutate(murders, rate = total / population * 10^5)
 # minimum, median, and maximum murder rate for the states in the West region using quantile
 # note that this returns a vector
 murders %>% 
+
+library(tidyverse)
+library(dplyr)
+library(dslabs)
+data(murders)
+murders <- mutate(murders, rate = total / population * 10^5)
+
+# average rate adjusted by population size
+us_murder_rate <- murders %>% 
+  summarize(rate = sum(total) / sum(population) * 10^5)
+us_murder_rate
+
+# us_murder_rate is stored as a data frame
+class(us_murder_rate)
+
+# the pull function can return it as a numeric value
+us_murder_rate %>% pull(rate)
+
+# using pull to save the number directly
+us_murder_rate <- murders %>% 
+  summarize(rate = sum(total) / sum(population) * 10^5) %>%
+  pull(rate)
+us_murder_rate
+
+# us_murder_rate is now stored as a number
+class(us_murder_rate)
   filter(region == "West") %>%
   summarize(range = quantile(rate, c(0, 0.5, 1)))
 
