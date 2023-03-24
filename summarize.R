1e9f4c778f9c261ec6da0b9633c874862c37a5a9
POOJA HARIHAR <88924201+poojaharihar03@users.noreply.github.com>
2023-03-24 10:51:05 +0530

    Update summarize.R

--- summarize.R
+++ summarize.R
@@ -24,3 +24,23 @@ us_murder_rate <- murders %>%
   summarize(rate = sum(total) / sum(population) * 10^5)
 us_murder_rate
 
+library(tidyverse)
+library(dplyr)
+library(dslabs)
+data(murders)
+murders <- mutate(murders, rate = total / population * 10^5)
+
+# minimum, median, and maximum murder rate for the states in the West region using quantile
+# note that this returns a vector
+murders %>% 
+  filter(region == "West") %>%
+  summarize(range = quantile(rate, c(0, 0.5, 1)))
+
+# returning minimum, median, and maximum as a data frame
+my_quantile <- function(x){
+  r <-  quantile(x, c(0, 0.5, 1))
+  data.frame(minimum = r[1], median = r[2], maximum = r[3]) 
+}
+murders %>% 
+  filter(region == "West") %>%
+  summarize(my_quantile(rate))
